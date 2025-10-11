35f15b20816537f4bdaa3f7b3e9532199f0fae1c
Apoorv Yashraj <apoorvyash129@gmail.com>
2025-10-11 23:09:09 +0530

    Update clive2.2prealpha.py

--- clive2.2prealpha.py
+++ clive2.2prealpha.py
@@ -1,36 +1,54 @@
 import discord  
 from nospace import token 
+from nospace import serverID 
 from discord.ext import commands
 from discord import app_commands #importing two files from discord.py to enable slash commands
 
-class Client(discord.Client):
+class Client(commands.Bot): #it will be commands.Bot instead of discord.Clients to make it accept slash commands
 
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+
+        try:
+            guild = discord.Object(id=serverID)
+            synced = await self.tree.sync(guild=guild)
+            print(f'Sync Complete\n{len(synced)} command synced to guild {guild.id}') #trying to sync slash command
+
+        except Exception as e:
+            print(f'Error: {e}') #taking the error here
+
+
+
     async def on_message(self, message):
        print(f'Message from {message.author}: {message.content}')
        
        if message.author == self.user:  
-           return #stops the bot from replying to itself like whatif it uses it's trigger cmd, that'd cause it to stuck in a endless loop
+           return
        
        if message.content.startswith('^911'):
            await message.channel.send(f'https://tenor.com/view/cartoon-tom-and-jerry-on-phone-tom-fbi-gif-17292737')
-           #heheheheheh
+           
        
        
        
        if message.content.startswith('^hello'):
            await message.channel.send(f'hello {message.author}!')
-           #so if message content startswith ^hello, send hello(persons name) into that channel only.
-           #await: Pauses the function until the coroutine is done (in this case, sending the message).
 
 
  
 intents = discord.Intents.default()
 
 intents.message_content = True
+client = Client(command_prefix="^", intents=intents) #modified the previous command and added a command_prefix; for some reason we still have to include it even to run / cmds
 
-client = Client(intents=intents)
+
+GUILD_ID = discord.Object(id=serverID)
+
+
+@client.tree.command(name="run", description="Slash cmds run check!", guild=GUILD_ID)
+async def run(interaction: discord.Interaction):
+    await interaction.response.send_message("Slash cmds are working fine!") #making a slash command 
+
+
 
 client.run(token)
-
