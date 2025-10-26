e4ecafd781224f0e4916475efd4050cbfd009999
Apoorv Yashraj <apoorvyash129@gmail.com>
2025-10-27 00:40:31 +0530

    Add placeholders for server ID and YouTube API
    Added placeholders for server ID and YouTube API key.

--- nospace.py
+++ nospace.py
@@ -3,3 +3,6 @@ token = #your discord bot token goes here
 serverID = #your server ID goes here
 
 YoutubeAPI = #your youtube api v3 goes here from google cloud
+
+
+#It calls your tokens and APIs keeping them away from main code
